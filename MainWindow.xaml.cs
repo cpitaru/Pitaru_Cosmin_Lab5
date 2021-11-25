4df23199d38418eace0c908f1c865d5d90f56fe8
cpitaru <pitaru.cosmin@gmail.com>
2021-11-25 20:14:12 +0200

    Finished lab#5

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -36,6 +36,9 @@ namespace Pitaru_Cosmin_Lab5
             System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // customerViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource inventoryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("inventoryViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // inventoryViewSource.Source = [generic data source]
         }
     }
 }
